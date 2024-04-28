6a3d3130523e814a620aff807dfff99be13b4aef
deniss n1ce <deniss.d@list.ru>
2024-04-29 02:18:56 +0300

    Review

--- Domain/Interface/Repositories/IBaseRepositories.cs
+++ Domain/Interface/Repositories/IBaseRepositories.cs
@@ -7,6 +7,5 @@
         Task<TEntity> CreateAsync(TEntity entity,bool saveChanges);
         Task<TEntity> UpdateAsync(TEntity entity, bool saveChanges);
         Task<TEntity> RemoveAsync(TEntity entity, bool saveChanges);
-        void SaveChangesAsync();
     }
 }
