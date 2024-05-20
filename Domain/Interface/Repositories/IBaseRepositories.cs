dd7ee41f34be1c8ce537b8fa95fe0af9b7cd6b00
N1CE <deniss.d@list.ru>
2024-05-20 09:47:15 +0300

    AddComent

--- Domain/Interface/Repositories/IBaseRepositories.cs
+++ Domain/Interface/Repositories/IBaseRepositories.cs
@@ -2,9 +2,24 @@
 {
     public interface IBaseRepositories<TEntity>
     {
+        /// <summary>
+        /// Получить весь список объектов
+        /// </summary>
         IEnumerable<TEntity> GetAll();
-        Task<TEntity> CreateAsync(TEntity entity,bool saveChanges);
+        /// <summary>
+        /// Создать объект
+        /// </summary>
+        /// <param name="saveChanges">Сохранить изменения</param>
+        Task<TEntity> CreateAsync(TEntity entity, bool saveChanges);
+        /// <summary>
+        /// Обновить объект
+        /// </summary>
+        /// <param name="saveChanges">Сохранить изменения</param>
         Task<TEntity> UpdateAsync(TEntity entity, bool saveChanges);
+        /// <summary>
+        /// Удалить ебъект
+        /// </summary>
+        /// <param name="saveChanges">Сохранить изменения</param>
         Task<TEntity> RemoveAsync(TEntity entity, bool saveChanges);
     }
 }
